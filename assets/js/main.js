866e1ff12a3540feee075c33488265b366349a49
Bill Ochunke <97397022+Bitnumbry@users.noreply.github.com>
2022-05-31 08:33:48 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,8 +20,27 @@
 			xsmall:	'(max-width: 480px)'
 		});
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
+	// Play initial animations on page load. 
+
+window.onload = function () {
+  Particles.init({
+    selector: ".background"
+  });
+};
+const particles = Particles.init({
+  selector: ".background",
+  color: ["#03dac6", "#ff0266", "#000000"],
+  connectParticles: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      options: {
+        color: ["#faebd7", "#03dac6", "#ff0266"],
+        maxParticles: 43,
+        connectParticles: false	}
+}
+]
+});	$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);

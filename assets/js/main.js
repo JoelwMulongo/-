494a983f98b1235b6c3acfb4d2a48fc5db98bb80
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-05-31 11:26:58 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,25 +22,8 @@
 
 	// Play initial animations on page load. 
 
-window.onload = function () {
-  Particles.init({
-    selector: ".background"
-  });
-};
-const particles = Particles.init({
-  selector: ".background",
-  color: ["#03dac6", "#ff0266", "#000000"],
-  connectParticles: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      options: {
-        color: ["#faebd7", "#03dac6", "#ff0266"],
-        maxParticles: 43,
-        connectParticles: false	}
-}
-]
-});	$window.on('load', function() {
+
+	$window.on('load', function() {
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
